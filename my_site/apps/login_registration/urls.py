e02943168b790cf14a2292440dc9a7de1c6d6022
Bradley <brad.coffer@gmail.com>
2018-06-24 14:05:55 -0500

    ...

--- my_site/apps/login_registration/urls.py
+++ my_site/apps/login_registration/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url
-from . import views          
+from . import views
+
 urlpatterns = [
-    url(r'^$', views.index)  
-]             
+    url(r'^$', views.index),
+    url(r'^login/$', views.login),
+    url(r'^register/$', views.register),
+    url(r'^success/$', views.success),
+]
