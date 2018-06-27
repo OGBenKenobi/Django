00584307cf0a6db1ba2fb662d22acd394614d8ea
Bradley <brad.coffer@gmail.com>
2018-06-27 15:52:51 -0500

    ...

--- trip_pal/apps/travels/urls.py
+++ trip_pal/apps/travels/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     url(r'^travels/show/(?P<id>\d+)$', views.show),
     url(r'^create$', views.create), 
     url(r'^delete/(?P<id>\d+)$', views.destroy),
+    url(r'^logout/$', views.logout),
+    url(r'^join/(?P<id>\d+)/$', views.join),
+    url(r'^cancel/(?P<id>\d+)/$', views.cancel),
 ]
